4cd0e19c47050a1e001d48642c68fa1664db3ded
Replit user <>
2023-11-09 06:51:35 +0000

    --

---
+++ Fundamentos/prog.cpp
@@ -0,0 +1,12 @@
+/* Mi primer programa escrito en C++
+ Fecha:12-Agosto-2011*/
+#include<iostream>
+using namespace std;
+int main()
+ {
+/* Escribe un mensaje */ 
+cout <<  "Hola, mundo" << endl;
+
+	return 0;
+  }
+
