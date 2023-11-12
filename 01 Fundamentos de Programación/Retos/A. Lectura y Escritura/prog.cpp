0b9a98f8f87704d67e1a4810303c7a9537398ebd
gerardobeltran <contacto@gerardobeltran.mx>
2023-11-12 11:11:16 -0700

    --

---
+++ 01 Fundamentos de Programación/Retos/A. Lectura y Escritura/prog.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int min, seg;
+    // cout << "Indica los minutos: ";
+    cin >> min;
+    
+    seg = 60 * min;
+    // cout <<"En segundos son: ";
+    cout << seg << endl;
+    
+    
+    return 0;
+}
