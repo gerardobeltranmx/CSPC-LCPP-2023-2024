936b7b50c2beaff8493b46e2d910e6d8e147ad23
Gerardo Beltrán <contacto@gerardobeltran.mx>
2023-11-25 10:54:46 -0700

    --

---
+++ 02 Estructuras de Control Condicionales/Lecciones/C. El mayor de dos/prog.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+	int a, b;
+	
+	// cin >> a;
+	// cin >> b;
+	cin >> a >> b;
+
+	if (a>b)
+		cout << a << endl;
+	else if (b>a)
+		cout << b << endl;
+	else
+		cout << a << endl;		
+
+
+	return 0;
+}
