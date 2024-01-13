6ae590c71114228b7e7238fe2e42e996a9cb065e
Gerardo Beltrán <contacto@gerardobeltran.mx>
2024-01-13 11:02:18 -0700

    --

---
+++ 03 Estructuras de repeticion/H. Mensaje de Amor/prog.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+	int N,M;
+
+	cin >> M; // cantidad de mensajes
+
+	for (int l=0; l<M; l++){
+		cin >> N;	// cantidad de corazones
+		for (int c=0; c < N; c++ ){ 
+			cout << "<3" ; 
+		}
+		cout << endl;
+	}
+
+
+	return 0;
+}
